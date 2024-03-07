a71a11729553781999683281f7856b322c13bccc
srishti-23 <142992320+srishti-23@users.noreply.github.com>
2024-03-07 22:38:53 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import Welcome from "./Welcome";
 import { MdDownloadForOffline } from "react-icons/md";
 
-
-
 const Home = () => {
   return (
     <div
@@ -12,20 +10,19 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center  h-full px-4 md:flex-row ">
       <div>
-      {/* <h1 className="text-4xl font-bold underline font-signature text-slate-200 ">
-          Hi <Welcome text="My name is Srishti Tiwari" delay={100} />
-        </h1> */}
+     
         <div className="flex flex-col justify-center h-full">
         <h2 className="text-4xl sm:text-7xl font-semibold text-slate-100 my-4">I'm a Full Stack Web Developer (MERN) </h2>
         <p className="text-3xl font-medium Raleway text-slate-200 my-4" >
         <Welcome  text ="Welcome to Srishti's portfolio" delay={100}/></p>
         <div >
           <a className="group text-white w-fit  my-2 px-4 py-3 mx-auto flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 hover:scale-125 duration-200"
-           href={process.env.PUBLIC_URL + '/Srishti.pdf'} download="Srishti.pdf"> Resume
+           href={process.env.PUBLIC_URL + '/Srishti.pdf'} 
+           target="_blank"
+            rel="noopener noreferrer" > Resume
             <span className=" group-hover:scale-125 duration-200 ml-2 ">
             <MdDownloadForOffline size={20} />
             </span>
-            
           </a>
         </div>
         </div>
