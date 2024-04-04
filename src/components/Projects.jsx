2ff721d13a08e80884ea2530c46bbd3e0057216b
srishti-23 <142992320+srishti-23@users.noreply.github.com>
2024-04-04 21:34:00 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,31 +1,40 @@
-import React from "react";
-import todo from "../images/todo.jpg"
-import myntra from "../images/myntra-clone.png"
-import travel from "../images/travel.png"
-const project = [
+import React, { useState } from "react";
+import todo from "../images/todo.jpg";
+import myntra from "../images/myntra-clone.png";
+import travel from "../images/travel.png";
+
+const projectData = [
   {
     id: 1,
     src: todo,
-    href:'https://list23.netlify.app/'
+    l: "https://github.com/srishti-23/todolist/blob/main/README.md",
+    href: "https://list23.netlify.app",
+    details: "This is a todo list application.A todo List is a tool used to keep track of tasks that need to be completed .This todolist has been created using React Js and Tailwind CSS",
   },
   {
     id: 2,
     src: myntra,
-    child:myntra,
-    href:'https://cloneofmyntra.netlify.app/products'
-
+    l: "https://github.com/srishti-23/myntra-clone/blob/main/README.md",
+    href: "https://cloneofmyntra.netlify.app",
+    details:
+      "A Myntra clone is a web application that aims to replicate the functionality and user interface of the Myntra e-commerce platform.it is designed using React js ,cart is managed using redux and UI is managed using Tailwind CSS . ",
   },
   {
     id: 3,
     src: travel,
-    child:travel,
-    href:'https://exploretgotrip.netlify.app/'
-
+    l: "https://github.com/srishti-23/travel-website/blob/main/README.md",
+    href: "https://exploretgotrip.netlify.app/",
+    details: "A simple static travel website named as ExploreGo.his website has been created using HTML and CSS",
   },
- 
 ];
 
 const Projects = () => {
+  const [projectDetails, setProjectDetails] = useState(null);
+
+  const handleProjectClick = (details) => {
+    setProjectDetails(details);
+  };
+
   return (
     <div
       name="projects"
@@ -39,22 +48,39 @@ const Projects = () => {
           <p className="py-6">Here are some of my major projects</p>
         </div>
         <div className="w-full h-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center px-8 py-12 sm:px-0">
-          {project.map(({ id, src,href }) => (
+          {projectData.map(({ id, src, href, details,l }) => (
             <div
               key={id}
               className="shadow-md hover:scale-125 duration-500 py-2 rounded-lg border-black "
             >
-              <img src={src} alt="" className="w-60 mx-auto" />
-              <p className="flex items-center justify-center ">
-                <button className=" mx-auto px-6 py-3 m-4 duration-200 hover:scale-105 text-center">
-                <a
-                href={href}
-                className="flex justify-between items-center w-full text-white"
-              >
-              View
-              </a>
-                </button>
-              </p>
+              {projectDetails === details ? (
+                <div>
+                  <p>{details}<br></br>
+                 <a href={l}> Read more..</a></p>
+                  <button onClick={() => setProjectDetails(null)}>Close</button>
+                </div>
+              ) : (
+                <div>
+                  <img src={src} alt="" className="w-60 mx-auto" />
+                  <p className="flex items-center justify-center ">
+                  <button className=" mx-auto px-6 py-3 m-4 duration-200 hover:scale-105 text-center">
+                      <a
+                        href={href}
+                        className=" justify-between items-center w-full text-white space-x-2"
+                      >
+                        View
+                      </a>
+                    </button>
+                    <button
+                      className="mx-auto px-6 py-3 m-4 duration-200 hover:scale-105 text-center "
+                      onClick={() => handleProjectClick(details)}
+                    >
+                      Details
+                    </button>
+                
+                  </p>
+                </div>
+              )}
             </div>
           ))}
         </div>
